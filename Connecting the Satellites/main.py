ef108b4617261b8c2fa75b8a818cabec3e069009
RSUltimate137 <savlaprashant27@gmail.com>
2025-10-04 12:55:33 +0200

    Add files via upload

--- Connecting the Satellites/main.py
+++ Connecting the Satellites/main.py
@@ -8,6 +8,9 @@ WIDTH=600
 TITLE="Connecting the Satellites"
 
 num_of_sat = 8
+start_time=0
+end_time=0
+total_time=0
 
 sats = []
 
@@ -15,13 +18,18 @@ for i in range(0,num_of_sat):
     satellite = Actor("satellite")
     satellite.x=random.randint(50,WIDTH-100)
     satellite.y=random.randint(50,HEIGHT-100)
-
     sats.append(satellite)
+start_time=time.time()
 
 
 def draw():
+    screen.blit("space",(0,0))
+    number=1
     for i in sats:
-        print(i)
+        screen.draw.text(str(number),(i.pos[0],i.pos[1]+20))
+        i.draw()
+        number+=1
+    screen.draw.text(str(round(start_time,1)),(10,10))
 
 def update():
     pass
